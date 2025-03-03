095bb9db4e881ee0c050ec3cd52d1c346b100486
honggeun jo <honggeun.jo@inha.ac.kr>
2025-03-03 18:30:55 +0900

    update visual part

--- _visualize_surface.py
+++ _visualize_surface.py
@@ -1,13 +1,23 @@
 import open3d as o3d
 import trimesh
 
-def visualize_surface(surface: trimesh.Trimesh|list[trimesh.Trimesh])->None:
+def visualize_surface(surface: trimesh.Trimesh | list[trimesh.Trimesh], save_html: str = None) -> None:
     assert isinstance(surface, (trimesh.Trimesh, list)), 'surface must be a trimesh or a list of trimesh'
+    
     if isinstance(surface, trimesh.Trimesh):
         surface = [surface]
+
     surface_o3d = [_plot_surface(s) for s in surface]
-    o3d.visualization.draw_geometries(surface_o3d)
-        
+    
+    if save_html:
+        o3d.visualization.draw(surface_o3d)
+        vis = o3d.visualization.WebVisualizer()
+        vis.set(meshes=surface_o3d)
+        vis.export(save_html)
+        print(f"Visualization saved as {save_html}")
+    else:
+        o3d.visualization.draw_geometries(surface_o3d)
+
 def _plot_surface(surface: trimesh.Trimesh) -> o3d.geometry.TriangleMesh:
     """Convert a trimesh surface to an o3d mesh."""
     mesh = o3d.geometry.TriangleMesh()
